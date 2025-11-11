00f0d97efcd850f5cc208910a2c2bb7b042fc0f4
Vinicius <viniciusmoraeschaves@gmail.com>
2025-11-11 15:16:58 -0300

    add vendas struct

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct data{
+    int dia;
+    int mes;
+    int ano;
+}Data;
+
+typedef struct venda{
+    int id;
+    char cliente[50];
+    char vendedor[50];
+    char matricula[5];
+    Data transacao;
+    float valor;
+}Venda;
+
+void imprimeArvore(Arv * arvore){}
+
+int main(){
+
+
+
+    return 0;
+}
