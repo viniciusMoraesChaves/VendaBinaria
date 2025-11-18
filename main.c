1a2f40f899430173400e7f3169e36e3bb7385d44
JVMinelli <joaominelli77@gmail.com>
2025-11-18 14:59:19 -0300

    feat:Menu de escolhas das funções

--- main.c
+++ main.c
@@ -38,11 +38,55 @@ int main(){
         case 2:
             imprimirArvore(arvoreVendas,0);
             break;
+        case 3: {
+            int a=-1;
+            do{
+                printf("Deseja buscar as vendas pelo nome do vendedor ou pela matrícula?");
+                scanf("%d", &a);
+                limpaBuffer();
+            }while(a!=0 && a!=1);
+
+            if(a==0) {
+                printf("Qual o nome do vendedor?");
+                char nome[50];
+                fgets(nome,sizeof(nome),stdin);
+                nome[strcspn(nome, "\n")] = '\0';
+
+                buscaImprimeNome(arvoreVendas->raiz, nome);
+
+            }
+            if(a==1) {
+                printf("Qual a matrícula?");
+                char matricula[5];
+                fgets(matricula,sizeof(matricula),stdin);
+                matricula[strcspn(matricula, "\n")] = '\0'
+
+                buscaImprimeMatricula(arvoreVendas->raiz, matricula);
+
+            }
+            break;
+        }
+        case 4: {
+            float valor=0;
+            printf("Qual o Valor?");
+            scanf("%d", valor);
+            int b=-1;
+            do{
+                printf("Deseja buscar as vendas acima do valor(0) ou abaixo(1)?");
+                scanf("%d", &b);
+                limpaBuffer();
+            }while(b!=0 && b!=1);
+
+            vendasAcimaAbaixoValor(arvoreVendas->raiz, valor, b);
+        }
+        break;
+
+
         default:
             break;
         }
     }
-    
+
 
     return 0;
 }
